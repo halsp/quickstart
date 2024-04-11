f560a9be5ee94290ef54d0122c0a3210db1f3b61
hal-wang <hi@hal.wang>
2024-04-11 16:15:18 +0000

    publish

---
+++ assets/404-42eb3617.js
@@ -0,0 +1,2 @@
+import{_ as e,c,o as n}from"./index-f0bca4e0.js";const o={};function r(t,_){return n(),c("div",null,"404")}const a=e(o,[["render",r]]);export{a as default};
+//# sourceMappingURL=404-42eb3617.js.map
